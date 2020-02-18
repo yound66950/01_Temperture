879e9061a9e2ebc5cb6ea6288da1ec32194b51b1
yound66950 <yound66950@users.noreply.github.com>
2020-02-19 09:56:18 +1300

    0219

--- 05_Rounding_trial2.py
+++ 05_Rounding_trial2.py
@@ -1,4 +1,15 @@
 #display output using int / float
 
 
-to_round = []
+to_round = [1/1, 1/2, 1/3]
+print("**** Numbers to round ****")
+print(to_round)
+
+print()
+print("**** Rounded Numbers ****")
+
+for item in to_round:
+    if item%1 == 0:
+        print("{:.0f}".format(item))
+    else:
+        print("{:.1f}".format(item))
